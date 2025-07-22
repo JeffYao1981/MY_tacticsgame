fb896d1e13c4a406199e9bd3e0973c919c063cd7
JeffYao1981 <yaijeff15@gmail.com>
2025-07-22 19:45:07 +0800

    change turn

--- scene/state_machine/player_turn_state.gd
+++ scene/state_machine/player_turn_state.gd
@@ -1,12 +1,21 @@
 extends BaseState
 
 var listen_for_input: bool = false
+var go_to_enemy_turn:bool = false
+
 
 func on_state_enter() -> void:
-	print(state_name + "enter")
+	TurnManager.enemy_turn_started.connect(on_enemy_turn_started)
 	listen_for_input = true
+	go_to_enemy_turn = false
+
+func on_state_frame_update(delta:float) -> void:
+	if go_to_enemy_turn:
+		state_changed.emit("EnemyTurnState")
+
 	
 func on_state_exit() -> void:
+	TurnManager.enemy_turn_started.disconnect(on_enemy_turn_started)
 	listen_for_input = false
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -15,3 +24,10 @@ func _unhandled_input(event: InputEvent) -> void:
 	
 	if event.is_action_pressed("left_mouse_click"):
 		PlayerActionManager.try_perform_selected_action()
+
+
+func on_enemy_turn_started() ->void:
+	if PlayerActionManager.is_performing_action:
+		return
+	go_to_enemy_turn = true
+	
---
+++ scene/ui/turn_ui.gd
@@ -0,0 +1,21 @@
+extends MarginContainer
+
+@onready var end_turn_button: Button = $EndTurnButton
+@onready var turn_label: Label = $TurnLabel
+
+
+func _ready() -> void:
+	end_turn_button.pressed.connect(on_end_turn_button_pressed)
+	TurnManager.player_turn_started.connect(on_player_turn_started)
+	TurnManager.enemy_turn_started.connect(on_enemy_turn_started)
+	
+	
+	
+func on_end_turn_button_pressed() ->void:
+	TurnManager.start_enemy_turn()
+	
+func on_player_turn_started() ->void:
+	turn_label.text = "Player Turn"
+	
+func on_enemy_turn_started() ->void:
+	turn_label.text = "Enemy Turn"
