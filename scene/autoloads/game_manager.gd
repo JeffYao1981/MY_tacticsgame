0925c67ff3f185de4c813125c95c50fa7d9dc540
JeffYao1981 <yaijeff15@gmail.com>
2025-08-05 22:25:35 +0800

    Unit_seletion_ui

--- scene/autoloads/game_manager.gd
+++ scene/autoloads/game_manager.gd
@@ -1,5 +1,8 @@
 extends Node
 
+signal  game_over()
+signal  game_win()
+
 var player_units:Array[Unit]
 var enemy_units:Array[Unit]
 
@@ -9,7 +12,7 @@ func register_unit(unit:Unit) -> void:
 		enemy_units.append(unit)
 	else:
 		player_units.append(unit)
-
+	unit.unit_died.connect(on_unit_died)
 
 func unregister_unit(unit:Unit) ->void:
 	if unit.is_enemy:
@@ -19,3 +22,10 @@ func unregister_unit(unit:Unit) ->void:
 		if PlayerActionManager.selected_unit == unit and not player_units.is_empty():
 			PlayerActionManager.set_selected_unit(player_units[0])
 		
+func on_unit_died(unit:Unit) -> void:
+	if unit.is_enemy and enemy_units.is_empty():
+		print("GameWin")
+		game_win.emit()
+	if not unit.is_enemy and player_units.is_empty():
+		print("GameOver")
+		game_over.emit()
---
+++ scene/ui/game_end_ui.gd
@@ -0,0 +1,37 @@
+extends MarginContainer
+
+
+@onready var button: Button = $PanelContainer/MarginContainer/VBoxContainer/Button
+@onready var label: Label = $PanelContainer/MarginContainer/VBoxContainer/Label
+
+
+
+var game_finished:bool = false
+
+func _ready() -> void:
+	GameManager.game_win.connect(on_game_win)
+	GameManager.game_over.connect(on_game_over)
+	button.pressed.connect(on_exit_button_pressed)
+	
+	visible = false
+	
+func _input(event: InputEvent) -> void:
+	if game_finished:
+		return
+	if event.is_action_pressed("escape"):
+		label.text = "Return to Menu"
+		visible = !visible
+	
+func on_game_win() ->void:
+	label.text = "Game Win"
+	visible = true
+	game_finished = true
+	
+	
+func on_game_over() ->void:
+	label.text = "Game Over"
+	visible = true
+	game_finished = true
+	
+func on_exit_button_pressed() ->void:
+	print("Exit Game")
